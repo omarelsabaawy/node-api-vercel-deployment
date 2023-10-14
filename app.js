00befe27d8fd739182a7f14632071bdf595ff0dd
omarelsabaawy <omarelsabaawy77@gmail.com>
2023-10-14 10:44:58 +0200

    rollback

--- app.js
+++ app.js
@@ -2,80 +2,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 // const bcrypt = require('bcrypt');
-const connectToDatabase = require('./Config/db');
-const User = require('./Models/User');
-
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => {
-    connectToDatabase();
-    console.log(`API listening on PORT ${3000} `)
-})
-
-const generateToken = (id) => {
-    return jwt.sign({ id }, "dslkvjcdslkckdsmclkdsmlkm_dslkmcdlskmcdsklmldskmnclkm", {
-        expiresIn: "30d",
-    });
-};
-
-app.post('/new-user', async (req, res) => {
-    const { email, password, phoneNumber, location, longitude, latitude, avatar } = req.body;
-
-    if (!email || !password || !phoneNumber || !location || !longitude || !latitude || !avatar) {
-        return res.status(400).json({
-            status: false,
-            message: "All fields are required."
-        });
-    }
-
-    try {
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-            return res.status(409).json({
-                status: false,
-                message: "This email is attached to another user."
-            });
-        }
-
-        // const saltRounds = 10;
-        // const encryptedPassword = await bcrypt.hash(password, saltRounds);
-
-        const newUser = new User({
-            email,
-            password,
-            phoneNumber,
-            location,
-            longitude,
-            latitude,
-            avatar
-        });
-
-        await newUser.save();
-
-        return res.status(201).json({
-            status: true,
-            message: "User account successfully created.",
-            id: newUser._id,
-            email: newUser.email,
-            phoneNumber: newUser.phoneNumber,
-            avatar: newUser.avatar,
-            access_token: generateToken(newUser._id)
-        });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            status: false,
-            error: "Internal Server Error: There was an issue while creating the user account."
-        });
-    }
+app.listen(3001, () => {
+    console.log(`API listening on PORT ${3001} `)
 })
 
 app.get('/', (req, res) => {
